5ead28c482f23b1c76242a9b00ecc7ffb65f1665
Touhid Mia <hmtm.cse@gmail.com>
2018-08-18 11:03:07 +0600

    ReactJS Bismillah CLI

--- projects/reactjs-bismillah-cli/src/index.js
+++ projects/reactjs-bismillah-cli/src/index.js
@@ -1,3 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+    return (
+        <div>
+            <Welcome name="Adeeb" />
+            <Welcome name="Torikul" />
+            <Welcome name="HMTMCSE" />
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
