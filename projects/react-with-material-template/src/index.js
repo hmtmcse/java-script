fd7f2a8192d340bac3e48c18fd64ab3adabe5ae6
Touhid Mia <hmtm.cse@gmail.com>
2018-08-20 14:10:37 +0600

    React Template Bismillah

--- projects/react-with-material-template/src/index.js
+++ projects/react-with-material-template/src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginPage from './component/page/login-page';
 
+
 ReactDOM.render(<LoginPage />, document.getElementById('root'));
