0da4e43e774284a487b0a89262c1428ffddfe453
Touhid Mia <hmtm.cse@gmail.com>
2018-08-20 14:10:17 +0600

    React Template Bismillah

--- projects/react-with-material-template/src/index.js
+++ projects/react-with-material-template/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import LoginPage from './component/page/login-page';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
